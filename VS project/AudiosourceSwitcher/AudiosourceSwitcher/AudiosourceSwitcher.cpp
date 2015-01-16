37bc9edc0db00aacad6c6600acda6b4cd7749483
Sawny <sawny@wallstrom.it>
2015-01-16 23:21:22 +0100

    Init commit, basic skin done.

---
+++ VS project/AudiosourceSwitcher/AudiosourceSwitcher/AudiosourceSwitcher.cpp
@@ -0,0 +1,101 @@
+// AudiosourceSwitcher.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "AudiosourceSwitcher.h"
+#include <stdio.h>
+#include <wchar.h>
+#include <tchar.h>
+#include <string>
+#include "windows.h"
+#include "Mmdeviceapi.h"
+#include "PolicyConfig.h"
+#include "Propidl.h"
+#include "Functiondiscoverykeys_devpkey.h"
+
+#define MAX_LOADSTRING 100
+
+HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
+{
+	IPolicyConfigVista *pPolicyConfig;
+	ERole reserved = eConsole;
+
+	HRESULT hr = CoCreateInstance(__uuidof(CPolicyConfigVistaClient),
+		NULL, CLSCTX_ALL, __uuidof(IPolicyConfigVista), (LPVOID *)&pPolicyConfig);
+	if (SUCCEEDED(hr))
+	{
+		hr = pPolicyConfig->SetDefaultEndpoint(devID, reserved);
+		pPolicyConfig->Release();
+	}
+	return hr;
+}
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	int option = atoi((char*) lpCmdLine);
+	//if (option == 0) option = -1;
+
+	HRESULT hr = CoInitialize(NULL);
+	if (SUCCEEDED(hr))
+	{
+		IMMDeviceEnumerator *pEnum = NULL;
+		// Create a multimedia device enumerator.
+		hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
+			CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnum);
+		if (SUCCEEDED(hr))
+		{
+			IMMDeviceCollection *pDevices;
+			// Enumerate the output devices.
+			hr = pEnum->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pDevices);
+			if (SUCCEEDED(hr))
+			{
+				UINT count;
+				pDevices->GetCount(&count);
+				if (SUCCEEDED(hr))
+				{
+					for (int i = 0; i < count; i++)
+					{
+						IMMDevice *pDevice;
+						hr = pDevices->Item(i, &pDevice);
+						if (SUCCEEDED(hr))
+						{
+							LPWSTR wstrID = NULL;
+							hr = pDevice->GetId(&wstrID);
+							if (SUCCEEDED(hr))
+							{
+								IPropertyStore *pStore;
+								hr = pDevice->OpenPropertyStore(STGM_READ, &pStore);
+								if (SUCCEEDED(hr))
+								{
+									PROPVARIANT friendlyName;
+									PropVariantInit(&friendlyName);
+									hr = pStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
+									if (SUCCEEDED(hr))
+									{
+										// if no options, print the device
+										// otherwise, find the selected device and set it to be default
+										if (option == -1) sprintf("Audio Device %d: %ws\n", i, friendlyName.pwszVal);
+										if (i == option) SetDefaultAudioPlaybackDevice(wstrID);
+										PropVariantClear(&friendlyName);
+									}
+									pStore->Release();
+								}
+							}
+							pDevice->Release();
+						}
+					}
+				}
+				pDevices->Release();
+			}
+			pEnum->Release();
+		}
+	}
+
+	return 1337;
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+Headphone Speaker Switcher
+==========================
+
+Rainmeter skin used to switch between headphones / speakers. Works in Windows 7.
+
+The AudiosourceSwitcher.exe is based on code from this repo: [justinryder/AudioEndPointController](https://github.com/justinryder/AudioEndPointController).
+
+
+
+
+AudiosourceSwitcher.exe
+-----------------------
+
+```
+AudiosourceSwitcher.exe [audio device to switch to]
+
+AudiosourceSwitcher.exe         <-- switches to number 0
+AudiosourceSwitcher.exe 0       <-- switches to number 0
+AudiosourceSwitcher.exe 1       <-- switches to number 1
+```
+
+![Image of audio devices](sound-devices.png)
